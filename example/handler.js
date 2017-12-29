81f3e5668a683ad33456ca7bc91750ed24e59dbd
Nik Graf <nik@nikgraf.com>
2017-12-30 00:17:03 +0100

    add plugin boilerplate and example using the local plugin

---
+++ example/handler.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!'
+    })
+  };
+
+  callback(null, response);
+};
