ba7b04822cdba8e64ebfc063f4f868ecaba3ee3e
markvp <6936351+markvp@users.noreply.github.com>
2020-01-10 13:15:33 +0800

    Fix typos and linting
    Fix typos and linting

--- src/index.js
+++ src/index.js
@@ -872,10 +872,10 @@ class ServerlessAppsyncPlugin {
     const flattenedMappingTemplates = config.mappingTemplates
       .reduce((accumulator, currentValue) => accumulator.concat(currentValue), []);
     return flattenedMappingTemplates.reduce((acc, tpl) => {
-      const reqSuffix = tpl.kind === 'PIPELINE' ? "before" : "request";
-      const respSuffix = tpl.kind === 'PIPELINE' ? "after" : "response";
-      const reqTemplPath = path.join(config.mappingTemplatesLocation, tpl.request || `${tpl.type}.${tpl.field}.{reqSuffix}.vtl`);
-      const respTemplPath = path.join(config.mappingTemplatesLocation, tpl.response || `${tpl.type}.${tpl.field}.{respSuffix}.vtl`);
+      const reqSuffix = tpl.kind === 'PIPELINE' ? 'before' : 'request';
+      const respSuffix = tpl.kind === 'PIPELINE' ? 'after' : 'response';
+      const reqTemplPath = path.join(config.mappingTemplatesLocation, tpl.request || `${tpl.type}.${tpl.field}.${reqSuffix}.vtl`);
+      const respTemplPath = path.join(config.mappingTemplatesLocation, tpl.response || `${tpl.type}.${tpl.field}.${respSuffix}.vtl`);
       const requestTemplate = fs.readFileSync(reqTemplPath, 'utf8');
       const responseTemplate = fs.readFileSync(respTemplPath, 'utf8');
 
