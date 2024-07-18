be5f6cd6b6fcf226482d6c0f4122d1f5b434bd1f
timo92 <timo.neuhaeusser+github@gmail.com>
2024-07-18 10:46:46 +0200

    fix: support serverless >= 4.0.23 (#632)

--- src/resources/Schema.ts
+++ src/resources/Schema.ts
@@ -5,7 +5,6 @@ import { CfnResources } from '../types/cloudFormation';
 import { Api } from './Api';
 import { flatten } from 'lodash';
 import { parse, print } from 'graphql';
-import ServerlessError from 'serverless/lib/serverless-error';
 import { validateSDL } from 'graphql/validation/validate';
 import { mergeTypeDefs } from '@graphql-tools/merge';
 
@@ -50,7 +49,7 @@ export class Schema {
   valdiateSchema(schema: string) {
     const errors = validateSDL(parse(schema));
     if (errors.length > 0) {
-      throw new ServerlessError(
+      throw new this.api.plugin.serverless.classes.Error(
         'Invalid GraphQL schema:\n' +
           errors.map((error) => `     ${error.message}`).join('\n'),
       );
