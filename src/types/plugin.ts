32c9e0797ce883b631168b0288ecd62724243317
Pierre <pierre@lezan.pro>
2024-11-15 17:27:14 +0100

    wip

--- src/types/plugin.ts
+++ src/types/plugin.ts
@@ -9,10 +9,7 @@ import type {
   SyncConfig,
   DsHttpConfig,
   DsDynamoDBConfig,
-  DsRelationalDbCoas mentioned above, we should have 2 schemas: one for a usual appsync config (main api or single stack) 
-  // and one for "external API" which only accepts supporters fields.
-  // This way, authentication can remain required in single stack scenarios 
-  // onfig,
+  DsRelationalDbConfig,
   DsOpenSearchConfig,
   DsLambdaConfig,
   DsEventBridgeConfig,
@@ -27,66 +24,66 @@ export * from './common';
 //? If most of the parameters are ignored when using apiId 
 //? you should define type like this to avoid misusage of the config, 
 //? it can be nice for those who use this type in their serverless.ts:
-/* ts */`
-export type BaseAppSyncConfig = {
-  dataSources: Record<string, DataSourceConfig>;
-  resolvers: Record<string, ResolverConfig>;
-  pipelineFunctions: Record<string, PipelineFunctionConfig>;
-  substitutions?: Substitutions;
-  caching?: CachingConfig;
-};
+/* ts */ `
+  export type BaseAppSyncConfig = {
+    dataSources: Record<string, DataSourceConfig>;
+    resolvers: Record<string, ResolverConfig>;
+    pipelineFunctions: Record<string, PipelineFunctionConfig>;
+    substitutions?: Substitutions;
+    caching?: CachingConfig;
+  };
 
-export  type NewAppSyncConfig = BaseAppSyncConfig & {
-  name: string;
-  schema: string[];
-  authentication: Auth;
-  additionalAuthentications: Auth[];
-  domain?: DomainConfig;
-  apiKeys?: Record<string, ApiKeyConfig>;
-  xrayEnabled?: boolean;
-  logging?: LoggingConfig;
-  waf?: WafConfig;
-  tags?: Record<string, string>;
-};
+  export  type NewAppSyncConfig = BaseAppSyncConfig & {
+    name: string;
+    schema: string[];
+    authentication: Auth;
+    additionalAuthentications: Auth[];
+    domain?: DomainConfig;
+    apiKeys?: Record<string, ApiKeyConfig>;
+    xrayEnabled?: boolean;
+    logging?: LoggingConfig;
+    waf?: WafConfig;
+    tags?: Record<string, string>;
+  };
 
-export type ExistingAppSyncConfig = BaseAppSyncConfig & {
-  apiId: string | IntrinsicFunction;
-};
+  export type ExistingAppSyncConfig = BaseAppSyncConfig & {
+    apiId: string | IntrinsicFunction;
+  };
 
-export type AppSyncConfig = NewAppSyncConfig | ExistingAppSyncConfig;
+  export type AppSyncConfig = NewAppSyncConfig | ExistingAppSyncConfig;
 `;
 
 //? I agree on this and it joins what I commented earlier.
 //? The same should happen in the validation json schema.
 //? I would use something like a union.
-/* ts */`
-export type BaseAppSyncConfig = {
-  dataSources: Record<string, DataSourceConfig>;
-  resolvers: Record<string, ResolverConfig>;
-  pipelineFunctions: Record<string, PipelineFunctionConfig>;
-  substitutions?: Substitutions;
-  caching?: CachingConfig;
-};
+/* ts */ `
+  export type BaseAppSyncConfig = {
+    dataSources: Record<string, DataSourceConfig>;
+    resolvers: Record<string, ResolverConfig>;
+    pipelineFunctions: Record<string, PipelineFunctionConfig>;
+    substitutions?: Substitutions;
+    caching?: CachingConfig;
+  };
 
-export  type FullAppSyncConfig = BaseAppSyncConfig & {
-  name: string;
-  schema: string[];
-  authentication: Auth;
-  additionalAuthentications: Auth[];
-  domain?: DomainConfig;
-  apiKeys?: Record<string, ApiKeyConfig>;
-  xrayEnabled?: boolean;
-  logging?: LoggingConfig;
-  waf?: WafConfig;
-  tags?: Record<string, string>;
-};
+  export  type FullAppSyncConfig = BaseAppSyncConfig & {
+    name: string;
+    schema: string[];
+    authentication: Auth;
+    additionalAuthentications: Auth[];
+    domain?: DomainConfig;
+    apiKeys?: Record<string, ApiKeyConfig>;
+    xrayEnabled?: boolean;
+    logging?: LoggingConfig;
+    waf?: WafConfig;
+    tags?: Record<string, string>;
+  };
 
-export  type SharedAppSyncConfig = BaseAppSyncConfig & {
-  apiId: string;
-};
+  export  type SharedAppSyncConfig = BaseAppSyncConfig & {
+    apiId: string;
+  };
 
-export type AppSyncConfig = FullAppSyncConfig | SharedAppSyncConfig
-`//! (not tested, might need adjustments)
+  export type AppSyncConfig = FullAppSyncConfig | SharedAppSyncConfig
+`; //! (not tested, might need adjustments)
 
 export type AppSyncConfig = {
   name: string;
