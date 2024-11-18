f385b737fef03e4b9749350ccb955e07522c051a
Pierre <pierre@lezan.pro>
2024-11-18 08:19:30 +0100

    Added typegards in tests without changing their functionality

--- src/__tests__/getAppSyncConfig.test.ts
+++ src/__tests__/getAppSyncConfig.test.ts
@@ -9,53 +9,59 @@ test('returns basic config', async () => {
 
 describe('Schema', () => {
   it('should return the default schema', () => {
-    expect(
-      getAppSyncConfig({ ...basicConfig, schema: undefined }).schema,
-    ).toMatchSnapshot();
+    const config = getAppSyncConfig({ ...basicConfig, schema: undefined });
+    const schema = 'schema' in config ? config.schema : undefined;
+    expect(schema).toMatchSnapshot();
   });
 
   it('should return a single schema as an array', () => {
-    expect(
-      getAppSyncConfig({ ...basicConfig, schema: 'mySchema.graphql' }).schema,
-    ).toMatchSnapshot();
+    const config = getAppSyncConfig({
+      ...basicConfig,
+      schema: 'mySchema.graphql',
+    });
+    const schema = 'schema' in config ? config.schema : undefined;
+    expect(schema).toMatchSnapshot();
   });
 
   it('should return a schema array unchanged', () => {
-    expect(
-      getAppSyncConfig({
-        ...basicConfig,
-        schema: ['users.graphql', 'posts.graphql'],
-      }).schema,
-    ).toMatchSnapshot();
+    const config = getAppSyncConfig({
+      ...basicConfig,
+      schema: ['users.graphql', 'posts.graphql'],
+    });
+    const schema = 'schema' in config ? config.schema : undefined;
+    expect(schema).toMatchSnapshot();
   });
 });
 
 describe('Api Keys', () => {
   it('should not generate a default Api Key when auth is not API_KEY', () => {
-    expect(
-      getAppSyncConfig({ ...basicConfig, authentication: { type: 'AWS_IAM' } })
-        .apiKeys,
-    ).toBeUndefined();
+    const config = getAppSyncConfig({
+      ...basicConfig,
+      authentication: { type: 'AWS_IAM' },
+    });
+    const apiKeys = 'apiKeys' in config ? config.schema : undefined;
+    expect(apiKeys).toBeUndefined();
   });
 
   it('should generate api keys', () => {
-    expect(
-      getAppSyncConfig({
-        ...basicConfig,
-        apiKeys: [
-          {
-            name: 'John',
-            description: "John's key",
-            expiresAt: '2021-03-09T16:00:00+00:00',
-          },
-          {
-            name: 'Jane',
-            expiresAfter: '1y',
-          },
-          'InlineKey',
-        ],
-      }).apiKeys,
-    ).toMatchInlineSnapshot(`
+    const config = getAppSyncConfig({
+      ...basicConfig,
+      apiKeys: [
+        {
+          name: 'John',
+          description: "John's key",
+          expiresAt: '2021-03-09T16:00:00+00:00',
+        },
+        {
+          name: 'Jane',
+          expiresAfter: '1y',
+        },
+        'InlineKey',
+      ],
+    });
+    const apiKeys = 'apiKeys' in config ? config.schema : undefined;
+
+    expect(apiKeys).toMatchInlineSnapshot(`
       Object {
         "InlineKey": Object {
           "name": "InlineKey",
